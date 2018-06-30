94e0d4453aa2b47b2d936e47069da154eb5a5832
NAGIREDDY SAICHARAN REDDY <charan100reddy@gmail.com>
2018-06-30 11:55:34 +0530

    Update index.js

--- Week1/Day6/js-scope/index.js
+++ Week1/Day6/js-scope/index.js
@@ -1,4 +1,4 @@
-If we execute this Javascript, what will the browser's console show?
+If we execute this Javascript, what will the browsers console show?
 
   var text = 'outside';
 function logIt(){
@@ -7,8 +7,8 @@ function logIt(){
 };
 logIt();
 
-//Answer: undefined, text was a global variable intially but when the interpreter executes calls the logIt() method the variable text is 
-//declared without assignment as local variable because of declaration in the method.But the assignment of value to the variable is done 
+//Answer: undefined, text was a global variable intially but when the interpreter executes,it calls the logIt() method the variable text is 
+//declared without assignment as local variable because of its declaration in the method. But the assignment of value to the variable is done 
 //by "text='inside'" statement.So before this statement in this method the text variable is Undefined
 
 //It is interpreted as
